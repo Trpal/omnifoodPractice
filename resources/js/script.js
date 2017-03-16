a8d1d6fc5a162f054021588ffd4caa47a8b02b94
Joni Korpelin <jonikorpelin@gmail.com>
2017-03-16 12:13:29 +0200

    map added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-  // Sticky navigation
+  /* Sticky navigation */
   $('.js--section-features').waypoint(function(direction) {
     if(direction == "down") {
       $('nav').addClass('sticky');
@@ -37,5 +37,69 @@ $(document).ready(function() {
           window.location.hash = target;
       });
   });
+
+
+  /* animations on scroll */
+
+  $('.js--wp-1').waypoint(function(direction){
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '70%'
+  })
+
+  $('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '60%'
+  })
+
+  $('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '60%'
+  })
+
+  $('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  })
+
+
+  /*mobile nav*/
+
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
+
+
+  /* Maps */
+
+  var map = new GMaps({
+    div: '.map',
+    lat: 38.7436266,
+    lng: -9.08,
+    zoom: 12
+  });
+
+  map.addMarker({
+  lat: 38.743626,
+  lng: -9.160203,
+  title: 'Omnifood Lisbon',
+  infoWindow: {
+    content: '<p>Our Lisbon HQ</p>'
+  }
+});
+
 
 });
